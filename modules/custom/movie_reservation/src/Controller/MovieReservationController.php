1c1d1528925798c20a9b67839d09ad10227c819e
Tamara Acimovic <85027693+acimovicTamara@users.noreply.github.com>
2022-02-07 09:11:48 +0100

    16-solved blank rows

--- modules/custom/movie_reservation/src/Controller/MovieReservationController.php
+++ modules/custom/movie_reservation/src/Controller/MovieReservationController.php
@@ -8,15 +8,9 @@ class MovieReservationController  {
     public function showMovie() {
       $query = \Drupal::entityQuery('node')
         ->condition('type', 'movie');
-        
-
       $movies = \Drupal\node\Entity\Node::loadMultiple($query->execute());
-      
-      $x=1;
-      
-      
-
-        return array(
+                      
+       return array(
           '#theme' => 'movies-list',
           '#movies' => $movies,
           '#title' => 'Welcome to our movie reservation page'
